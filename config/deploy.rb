88ea932ea8f16b8c7a25e03a243765bd92228cfd
Your Name <you@example.com>
2018-02-14 23:39:55 +0300

    secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/playlister/st-admin"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/secrets.yml" #"config/database.yml",
+append :linked_files, "config/secrets.yml" #"config/database.yml",
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads"
